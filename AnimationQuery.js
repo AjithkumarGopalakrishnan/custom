d8c6614dc044c5fae3f879d1622bf3789036d7a2
AjithkumarGopalakrishnan <93252370+AjithkumarGopalakrishnan@users.noreply.github.com>
2023-09-28 11:19:04 +0530

    Update AnimationQuery.js

--- AnimationQuery.js
+++ AnimationQuery.js
@@ -1,12 +1,8 @@
 function animationShow(element) {
-    var gantt = document.getElementById(element);
-    gantt.querySelector('.e-chart-scroll-container').classList.add('e-gantt-show');
-    gantt.querySelector('.e-gantt-skeleton').style.visibility = "visible";
-    gantt.querySelector('.e-gridcontent').classList.add('e-gantt-show');
-}
-function animationHide(element) {
-      var gantt = document.getElementById(element);
-    gantt.querySelector('.e-gantt-skeleton').style.visibility = "hidden";
-    gantt.querySelector('.e-gridcontent').classList.remove('e-gantt-show');
-    gantt.querySelector('.e-chart-scroll-container').classList.remove('e-gantt-show');
-}
+            var gantt = document.getElementById(element);
+            gantt.classList.add('e-gantt-show');
+        }
+        function animationHide(element) {
+            var gantt = document.getElementById(element);
+            gantt.classList.remove('e-gantt-show');
+        }
