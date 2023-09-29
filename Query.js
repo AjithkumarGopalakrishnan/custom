151552adeb97cdb4d80213b872d334bc12f9eafd
AjithkumarGopalakrishnan <93252370+AjithkumarGopalakrishnan@users.noreply.github.com>
2023-09-29 19:57:54 +0530

    Create Query.js

---
+++ Query.js
@@ -0,0 +1,6 @@
+function animationShow(element) {
+  document.getElementById(element).classList.add('e-gantt-show');
+}
+function animationHide(element) {
+  document.getElementById(element).classList.remove('e-gantt-show');
+}
