c63988deec9d2f82ddb08e79a34f965baddae37e
AjithkumarGopalakrishnan <93252370+AjithkumarGopalakrishnan@users.noreply.github.com>
2023-12-14 12:45:53 +0530

    Update GanttQuery.js

--- GanttQuery.js
+++ GanttQuery.js
@@ -1,8 +1,34 @@
- function animationShow(element) {
-            var gantt = document.getElementById(element);
-            gantt.classList.add('e-gantt-show');
-        }
-        function animationHide(element) {
-            var gantt = document.getElementById(element);
-            gantt.classList.remove('e-gantt-show');
-        }
+ var ganttChartContainer = null;
+var orginalElement = null;
+var taskbarEditState = false;
+
+function bindGanttChartEvent() {
+    ganttChartContainer = window.sfBlazor.Gantt.instance.taskbarEditModule.chartPane;
+    sf.base.EventHandler.add(ganttChartContainer, sf.base.Browser.touchStartEvent, ganttMouseDownHandler, this);
+    sf.base.EventHandler.add(ganttChartContainer, sf.base.Browser.touchEndEvent, ganttMouseUpHandler, this);
+}
+
+function destroyGanttChartEvent() {
+    sf.base.EventHandler.remove(ganttChartContainer, sf.base.Browser.touchStartEvent, ganttMouseDownHandler);
+    sf.base.EventHandler.remove(ganttChartContainer, sf.base.Browser.touchEndEvent, ganttMouseUpHandler);
+}
+function ganttMouseDownHandler(e) {
+    if (e.target.closest(".e-taskbar-main-container") != null) {
+        sf.base.EventHandler.add(ganttChartContainer, sf.base.Browser.touchMoveEvent, ganttMouseMoveHandler, this);
+    }
+}
+function ganttMouseMoveHandler(e) {
+    var cloneElement = document.querySelectorAll(".e-gantt-clone-taskbar");
+    if (cloneElement != null && cloneElement != "undefined" && cloneElement.length > 0 && !taskbarEditState) {
+        orginalElement = cloneElement[0].closest(".e-chart-row-cell").querySelectorAll(".e-taskbar-main-container");
+        orginalElement[0].style.visibility = "hidden";
+        taskbarEditState = true;
+    }
+}
+function ganttMouseUpHandler(e) {
+    taskbarEditState = false;
+    if (orginalElement != null && orginalElement != "undefined" && orginalElement.length > 0) {
+        orginalElement[0].style.visibility = "visible";
+    }
+    sf.base.EventHandler.remove(ganttChartContainer, sf.base.Browser.touchMoveEvent, ganttMouseMoveHandler);
+}
